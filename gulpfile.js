8da2a373f7721d36c5677813c03d7fba33f90aca
Silas Tippens <silas@level2d.com>
2016-12-06 20:58:07 -0500

    fix gulp upload

--- gulpfile.js
+++ gulpfile.js
@@ -156,7 +156,7 @@ gulp.task('handlebars', function (done) {
  */
 gulp.task('upload', function () {
     var creds = readAsJSON('./.ftpdest');
-	return gulp.src('dest/**/*')
+	return gulp.src('dist/**/*')
 		.pipe(sftp({
 			host: creds.host,
 			auth: 'poetKey',
