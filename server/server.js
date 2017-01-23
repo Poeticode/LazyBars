fa87286012e47789cb810ca5741da047e65ccb1c
Poeticode <Silas@level2d.com>
2017-01-23 10:33:12 -0500

    change port

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 app
     .use(express.static('dist'))
     .use('/dynamic', express.static(path.join('_src','/hbs','/data', '/dynamic')))
-    .listen(8080);
+    .listen(23485);
 
 
 
